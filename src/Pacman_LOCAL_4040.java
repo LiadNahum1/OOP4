a8bd13cd5b6da379cd3ac4fc1e66cf9601e6e776
abensour <37442472+abensour@users.noreply.github.com>
2018-06-01 01:57:30 +0300

    Merge commit 'e8f3a86db86fb0fc5701dc1d61bfaf6ffe939d2d' into HEAD
    # Conflicts:
    #	src/Game.java
    #	src/Pacman.java

---
+++ src/Pacman_LOCAL_4040.java
@@ -0,0 +1,21 @@
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+
+public abstract class Pacman implements Visited{
+
+	
+public abstract void impact(Visitor v);
+
+public void move() {
+	// TODO Auto-generated method stub
+	
+}
+
+public void draw(Board game, Graphics g) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+}
