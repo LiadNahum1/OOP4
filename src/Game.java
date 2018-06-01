1d2abdbb5cd5ca01d6954feb52fa3e646fe3b914
abensour <37442472+abensour@users.noreply.github.com>
2018-06-02 01:43:11 +0300

    save

--- src/Game.java
+++ src/Game.java
@@ -1,26 +1,22 @@
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.geom.RoundRectangle2D;
 import java.util.Vector;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Game extends JFrame implements ActionListener, KeyListener {
 	private BoardTile [][] boardTiles;
 	private String [][] boardTilesS; 
-	private Vector [][] pDirection; //possible directions
 	private Timer timer;
+	private Vector<Pair> [][] neighbors;
 	private int level; 
 	private Pacman pacman;
 	private boolean start; 
@@ -34,14 +30,39 @@ public class Game extends JFrame implements ActionListener, KeyListener {
 		this.setSize(800,800);
 		this.start = true; 
 		this.timer = new Timer(500, this);
-
 		if(level == 1) {
 			this.pacman = new NicePacman(new Pair(400,400)); 
 		}
 		initializeBoardTilesS();
 		initializeBoard();
+		inisializeNeighborsMat();
 		this.addKeyListener(this);
 		this.setVisible(true);
+	}
+	@SuppressWarnings("unchecked")
+	private void inisializeNeighborsMat() {
+		neighbors=(Vector<Pair>[][]) new Vector[32][32];
+		for(int i=0;i<32;i++) {
+			for(int j=0;j<32;j++) {
+				neighbors[i][j]=findneighbors(i,j);
+			}
+		}
+	}
+	private Vector<Pair> findneighbors(int curY,int curX) {
+		if(this.boardTilesS[curY][curX] == "w")
+			return null;
+		else {
+			Vector <Pair> possibleDirs = new Vector <>();
+			if(this.boardTilesS[curY+1][curX]!="w") //can move up
+				possibleDirs.add(new Pair(curX,curY+1));
+			if(this.boardTilesS[curY-1][curX]!="w")//can move down
+				possibleDirs.add(new Pair(curX,curY-1));
+			if(this.boardTilesS[curY][curX+1]!="w")//can move right
+				possibleDirs.add(new Pair(curX+1,curY));
+			if(this.boardTilesS[curY][curX-1]!="w")//can move left
+				possibleDirs.add(new Pair(curX-1,curY));
+			return possibleDirs;
+		}
 	}
 	private void initializeBoard() {
 		Container cp= this.getContentPane();
@@ -49,7 +70,6 @@ public class Game extends JFrame implements ActionListener, KeyListener {
 		for(int i=0;i<32;i++){
 			for(int j=0;j<32;j++){
 				initializeBoardTile(i,j);
-				System.out.println(i+""+j);
 				cp.add(boardTiles[i][j]);
 			}
 		}
@@ -57,100 +77,101 @@ public class Game extends JFrame implements ActionListener, KeyListener {
 
 
 
-		private void initializeBoardTile (int x,int y) {
-			if(boardTilesS [x][y] == "w")
-				boardTiles[x][y] =new WallTile(this.level);
-			if(boardTilesS [x][y] == "d")
-				boardTiles[x][y] =new RoadTile(false);
-			if(boardTilesS [x][y] == "0")
-				boardTiles[x][y] =new RoadTile(true);
-			if(boardTilesS[x][y] == "g")
-				boardTiles [x][y] = new GateTile();
+	private void initializeBoardTile (int x,int y) {
+		if(boardTilesS [x][y] == "w")
+			boardTiles[x][y] =new WallTile(this.level);
+		if(boardTilesS [x][y] == "d")
+			boardTiles[x][y] =new RoadTile(false);
+		if(boardTilesS [x][y] == "0")
+			boardTiles[x][y] =new RoadTile(true);
+		if(boardTilesS[x][y] == "g")
+			boardTiles [x][y] = new GateTile();
+	}
+
+	public static void main(String[]args) {
+		new Game(1);
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(this.timer)) {
+			if(!this.start)
+				this.pacman.move();
+			repaint();
+		}
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+			this.timer.start();
+		}
+		else {
+			this.pacman.manageMovement(e);
+			repaint();
 		}
 
-				public static void main(String[]args) {
-			new Game(1);
-		}
+	}
 
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(e.getSource().equals(this.timer)) {
-				if(!this.start)
-					this.pacman.move();
-				repaint();
-			}
-		}
-		@Override
-		public void keyPressed(KeyEvent e) {
-			if(e.getKeyCode() == KeyEvent.VK_SPACE) {
-				this.timer.start();
-			}
-			else {
-				this.pacman.manageMovement(e);
-				repaint();
-			}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
 
-		}
+	}
 
 
-		@Override
-		public void keyReleased(KeyEvent arg0) {
-			// TODO Auto-generated method stub
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
 
-		}
+	}
+	@Override
+	public void paint(Graphics g){
+		Image im = this.pacman.draw(this, g);
+		Pair position = this.pacman.getCurrentPosition();
+		g.drawImage(im,position.getX(), position.getY(), this);
+	}
+	public void initializeBoardTilesS() {
+		this.boardTilesS = new String[][] 
+				{{"w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w"},
+			{"w", "d","d","d","d","d","d","d","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","d","d","d","d","d","d","d","w"},
+			{"w", "d","w","w","w","w","d","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","d","w","w","w","w","d","w"},
+			{"w", "d","w","w","w","w","d","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","d","w","w","w","w","d","w"},
+			{"w", "d","w","w","w","w","d","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","d","w","w","w","w","d","w"},
+			{"w", "d","w","w","w","w","d","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","d","w","w","w","w","d","w"},
+			{"w", "d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","w"},
+			{"w", "d","w","w","w","w","d","w","d","w","w","w","w","w","w","w","w","w","w","w","w","w","w","d","w","d","w","w","w","w","d","w"},
+			{"w", "d","w","w","w","w","d","w","d","w","w","w","w","w","w","w","w","w","w","w","w","w","w","d","w","d","w","w","w","w","d","w"},
+			{"w", "d","d","d","d","d","d","w","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","w","d","w","w","w","w","d","w"},
+			{"w", "w","w","w","w","w","d","w","w","w","w","w","w","w","0","w","w","0","w","w","w","w","w","w","w","d","w","w","w","w","w","w"},
+			{"w", "w","w","w","w","w","d","w","w","w","w","w","w","w","0","w","w","0","w","w","w","w","w","w","w","d","w","w","w","w","w","w"},
+			{"w", "w","w","w","w","w","d","w","0","0","0","0","0","0","0","w","w","0","0","0","0","0","0","0","w","d","w","w","w","w","w","w"},
+			{"w", "w","w","w","w","w","d","w","0","w","w","w","w","w","0","w","w","0","w","w","w","w","w","0","w","d","w","w","w","w","w","w"},
+			{"w", "w","w","w","w","w","d","w","0","w","w","w","0","0","0","0","0","0","0","0","w","w","w","0","w","d","w","w","w","w","w","w"},
+			{"w", "w","w","w","w","w","d","0","0","w","w","w","0","w","w","g","g","w","w","0","w","w","w","0","w","0","w","w","w","w","w","w"},
+			{"w", "w","w","w","w","w","d","w","0","w","w","w","0","w","w","w","w","w","w","0","w","w","w","0","w","d","w","w","w","w","w","w"},
+			{"w", "w","w","w","w","w","d","w","0","w","w","w","0","0","0","0","0","0","0","0","w","w","w","0","w","d","w","w","w","w","w","w"},
+			{"w", "w","w","w","w","w","d","w","0","w","w","w","w","w","w","w","w","w","w","w","w","w","w","0","w","d","w","w","w","w","w","w"},
+			{"w", "w","w","w","w","w","d","w","0","w","w","w","w","w","w","w","w","w","w","w","w","w","w","0","w","d","w","w","w","w","w","w"},
+			{"w", "d","d","d","d","d","d","d","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","d","d","d","d","d","d","d","w"},
+			{"w", "d","w","w","w","w","d","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","d","w","w","w","w","d","w"},
+			{"w", "d","w","w","w","w","d","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","d","w","w","w","w","d","w"},
+			{"w", "d","d","d","w","w","d","d","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","d","d","w","w","d","d","d","w"},
+			{"w", "w","w","d","w","w","d","w","d","w","w","w","w","w","w","w","w","w","w","w","w","w","w","d","w","d","w","w","d","w","w","w"},
+			{"w", "d","d","d","w","w","d","w","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","w","d","w","w","d","d","d","w"},
+			{"w", "w","w","d","w","w","d","w","d","w","w","w","w","w","d","w","w","d","w","w","w","w","w","d","w","d","w","w","d","w","w","w"},
+			{"w", "d","d","d","d","d","d","w","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","w","d","d","d","d","d","d","w"},
+			{"w", "d","w","w","w","w","w","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","w","w","w","w","w","d","w"},
+			{"w", "d","w","w","w","w","w","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","w","w","w","w","w","d","w"},
+			{"w", "d","d","d","d","d","d","d","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","d","d","d","d","d","d","d","w"},
+			{"w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w"}};
 
-
-		@Override
-		public void keyTyped(KeyEvent arg0) {
-			// TODO Auto-generated method stub
-
-		}
-		public void paintComponent(Graphics g){
-			Image im = this.pacman.draw(this, g);
-			Pair position = this.pacman.getCurrentPosition();
-			g.drawImage(im,position.getX(), position.getY(), this);
-		}
-		public void initializeBoardTilesS() {
-			this.boardTilesS = new String[][] 
-					{{"w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w"},
-				{"w", "d","d","d","d","d","d","d","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","d","d","d","d","d","d","d","w"},
-				{"w", "d","w","w","w","w","d","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","d","w","w","w","w","d","w"},
-				{"w", "d","w","w","w","w","d","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","d","w","w","w","w","d","w"},
-				{"w", "d","w","w","w","w","d","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","d","w","w","w","w","d","w"},
-				{"w", "d","w","w","w","w","d","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","d","w","w","w","w","d","w"},
-				{"w", "d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","w"},
-				{"w", "d","w","w","w","w","d","w","d","w","w","w","w","w","w","w","w","w","w","w","w","w","w","d","w","d","w","w","w","w","d","w"},
-				{"w", "d","w","w","w","w","d","w","d","w","w","w","w","w","w","w","w","w","w","w","w","w","w","d","w","d","w","w","w","w","d","w"},
-				{"w", "d","d","d","d","d","d","w","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","w","d","w","w","w","w","d","w"},
-				{"w", "w","w","w","w","w","d","w","w","w","w","w","w","w","0","w","w","0","w","w","w","w","w","w","w","d","w","w","w","w","w","w"},
-				{"w", "w","w","w","w","w","d","w","w","w","w","w","w","w","0","w","w","0","w","w","w","w","w","w","w","d","w","w","w","w","w","w"},
-				{"w", "w","w","w","w","w","d","w","0","0","0","0","0","0","0","w","w","0","0","0","0","0","0","0","w","d","w","w","w","w","w","w"},
-				{"w", "w","w","w","w","w","d","w","0","w","w","w","w","w","0","w","w","0","w","w","w","w","w","0","w","d","w","w","w","w","w","w"},
-				{"w", "w","w","w","w","w","d","w","0","w","w","w","0","0","0","0","0","0","0","0","w","w","w","0","w","d","w","w","w","w","w","w"},
-				{"w", "w","w","w","w","w","d","0","0","w","w","w","0","w","w","g","g","w","w","0","w","w","w","0","w","0","w","w","w","w","w","w"},
-				{"w", "w","w","w","w","w","d","w","0","w","w","w","0","w","w","w","w","w","w","0","w","w","w","0","w","d","w","w","w","w","w","w"},
-				{"w", "w","w","w","w","w","d","w","0","w","w","w","0","0","0","0","0","0","0","0","w","w","w","0","w","d","w","w","w","w","w","w"},
-				{"w", "w","w","w","w","w","d","w","0","w","w","w","w","w","w","w","w","w","w","w","w","w","w","0","w","d","w","w","w","w","w","w"},
-				{"w", "w","w","w","w","w","d","w","0","w","w","w","w","w","w","w","w","w","w","w","w","w","w","0","w","d","w","w","w","w","w","w"},
-				{"w", "d","d","d","d","d","d","d","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","d","d","d","d","d","d","d","w"},
-				{"w", "d","w","w","w","w","d","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","d","w","w","w","w","d","w"},
-				{"w", "d","w","w","w","w","d","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","d","w","w","w","w","d","w"},
-				{"w", "d","d","d","w","w","d","d","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","d","d","w","w","d","d","d","w"},
-				{"w", "w","w","d","w","w","d","w","d","w","w","w","w","w","w","w","w","w","w","w","w","w","w","d","w","d","w","w","d","w","w","w"},
-				{"w", "d","d","d","w","w","d","w","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","w","d","w","w","d","d","d","w"},
-				{"w", "w","w","d","w","w","d","w","d","w","w","w","w","w","d","w","w","d","w","w","w","w","w","d","w","d","w","w","d","w","w","w"},
-				{"w", "d","d","d","d","d","d","w","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","w","d","d","d","d","d","d","w"},
-				{"w", "d","w","w","w","w","w","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","w","w","w","w","w","d","w"},
-				{"w", "d","w","w","w","w","w","w","w","w","w","w","w","w","d","w","w","d","w","w","w","w","w","w","w","w","w","w","w","w","d","w"},
-				{"w", "d","d","d","d","d","d","d","d","d","d","d","d","d","d","w","w","d","d","d","d","d","d","d","d","d","d","d","d","d","d","w"},
-				{"w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w"}};
-
-		}
+	}
 
 }
 
-	/*	private void drawBoard(Graphics g) {
+/*	private void drawBoard(Graphics g) {
 	for(int y=0; y<32;y++) {
 		for(int x=0; x<32;x++) {
 			if(boardTilesS [y][x] == "w") {
@@ -162,8 +183,8 @@ public class Game extends JFrame implements ActionListener, KeyListener {
 					g.drawImage(roads[1],x*25,(y+1)*25,this);
 				else if(boardTilesS[y][x] == "g")
 					g.drawImage(gate,x*25,(y+1)*25,this);	
-			
+
 			}
 		}	
 	}
-	*/
+ */
--- src/Ghost.java
+++ src/Ghost.java
@@ -12,35 +12,11 @@ public abstract class Ghost  implements Visitor {
 		pacman.impact(this);
 	}
 	public Pair move(String board [][]) {
-		Vector <Pair> possibleDirs = getPosDirs(board);
-		if(possibleDirs.size()==1) {
-			return possibleDirs.get(0);
-		}
-		else {
-	//	int	i = calculateMovesToCatch(currentPosition,possibleDirs);
-		}
+
 	return null;	
 
 	}
-
 	
-			
-			
-	
-	private Vector<Pair> getPosDirs(String[][] board) {
-		Vector <Pair> possibleDirs = new Vector <>();
-		int curX = currentPosition.getX()/25;
-		int curY = currentPosition.getX()/25;
-		if(board[curY+1][curX]!="w"& curDir!=1) //can move up
-			possibleDirs.add(new Pair(curX,curY+1));
-		if(board[curY-1][curX]!="w"& curDir!=0)//can move down
-			possibleDirs.add(new Pair(curX,curY-1));
-		if(board[curY][curX+1]!="w"& curDir!=2)//can move right
-			possibleDirs.add(new Pair(curX+1,curY));
-		if(board[curY][curX-1]!="w"& curDir!=3)//can move left
-			possibleDirs.add(new Pair(curX-1,curY));
-		return possibleDirs;
-	}
 	public abstract void visit(NicePacman pacman);
 	public abstract void visit(DefendedPacman pacman);
 	public abstract void visit(AngryPacman pacman);
